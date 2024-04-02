06e68b517ff5ecf9c9763402d051558dcbdf2d30
Souvik De <souvik.de@sato-global.com>
2024-04-02 21:52:07 +0900

    add movie rows to homescreen

--- netflix-app/src/Nav.js
+++ netflix-app/src/Nav.js
@@ -2,34 +2,35 @@ import React, { useEffect, useState } from 'react';
 import "./Nav.css";
 
 function Nav() {
-const {show,handleShow} = useState(false);
-const transitionNavBar = () =>{
-    if(window.scrollY > 100){
-        handleShow(true);
-    }else{
-        handleShow(false);
+    const [show, setShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
     }
-}
 
-useEffect(() => {
-    window.addEventListener("scroll", transitionNavBar);
-    return () => window.removeEventListener('scroll',transitionNavBar);
-}, []);
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener('scroll', transitionNavBar);
+    }, []);
 
-  return (
-    <div className={`nav ${show && "nav__black"}`}>
-        <div className='nav__contents'>
-            <img className='nav__logo'
-                src="https://bi-jingo.com/wp-content/uploads/2009/03/215-2154144_transparent-background-netflix-logo.jpg"
-                alt=""
-            />
-            <img className='nav__avatar'
-                src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
-                alt=''
-            />
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <div className='nav__contents'>
+                <img className='nav__logo'
+                    src="https://bi-jingo.com/wp-content/uploads/2009/03/215-2154144_transparent-background-netflix-logo.jpg"
+                    alt=""
+                />
+                <img className='nav__avatar'
+                    src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+                    alt=''
+                />
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
 export default Nav;
