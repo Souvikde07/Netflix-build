e60e87e458da6b4b9314e27a8daf85e6ceac0695
Souvik De <souvik.de@sato-global.com>
2024-04-04 17:19:43 +0900

    add firebase.js and install react-router-dom for routing

---
+++ netflix-app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCClNK7uR6_2FOB_jmlFqbzUpVItiBtagE",
+    authDomain: "netflix-build-d0a29.firebaseapp.com",
+    projectId: "netflix-build-d0a29",
+    storageBucket: "netflix-build-d0a29.appspot.com",
+    messagingSenderId: "988961698115",
+    appId: "1:988961698115:web:e77871265eed690bca7ff6"
+  };
